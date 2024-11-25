39e70ea55df82de6c9f2b4270cd848ccb43817c9
unknown <HP@WFH20-NIF0623214.NIFTEL.LOCAL>
2024-11-25 22:09:15 +0530

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['@mui/icons-material', '@mui/icons-material/WhatsApp'],
+      external: ['react-slick'],
     },
   },
 });
